ca4330681f22b2fa6a85e5ff4bbdd2d8d47b142e
JINJU PARK <aricclunj@naver.com>
2025-03-26 19:48:59 +0900

    togemory로 변경

--- src/main/java/com/jinju/TogemoryApplication.java
+++ src/main/java/com/jinju/TogemoryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TogetheryApplication {
+public class TogemoryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TogetheryApplication.class, args);
+		SpringApplication.run(TogemoryApplication.class, args);
 	}
 
 }
