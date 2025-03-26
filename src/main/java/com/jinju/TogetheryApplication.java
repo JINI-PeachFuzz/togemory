12d5b323b608f60c1cf7e3408e11486f9824574b
JINJU PARK <aricclunj@naver.com>
2025-03-26 17:49:02 +0900

    초기 업로드

---
+++ src/main/java/com/jinju/TogetheryApplication.java
@@ -0,0 +1,13 @@
+package com.jinju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetheryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetheryApplication.class, args);
+	}
+
+}
