90286a861b8b4d5ebc5298af916014b5b9e69f1d
JINJU PARK <aricclunj@naver.com>
2025-04-20 22:52:50 +0900

    .

--- src/main/java/org/jinju/global/configs/WebSocketConfig.java
+++ src/main/java/org/jinju/global/configs/WebSocketConfig.java
@@ -2,6 +2,7 @@ package org.jinju.global.configs;
 
 import org.jinju.message.websockets.MessageHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -14,15 +15,15 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Autowired
     private MessageHandler messageHandler;
 
+    @Value("${spring.profiles.active:default}")
+    private String profile;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // ws://localhost:3000/message
-        String profile = System.getenv("spring.profiles.active");
+        // ws://localhost:8000/message
+//        String profile = System.getenv("spring.profiles.active"); //운영체제의 환경 변수를 읽는 거
 
-
-        registry.addHandler(messageHandler, "msg")
-                .setAllowedOrigins(profile.contains("prod") ? "" : "http://localhost:3000");
-
-
+        registry.addHandler(messageHandler, "/msg")
+                .setAllowedOrigins(profile.contains("prod") ? "https://togemory.jinilog.com" : "http://localhost:8000");
     }
 }
